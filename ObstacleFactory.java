c70e22013b4dfb0e2d6967e304e8c5255fa609e7
Matteo Camanni <Matteosystem@MacBookCamanni2.station>
2017-12-29 14:16:30 +0100

    movement improvement

--- ObstacleFactory.java
+++ ObstacleFactory.java
@@ -16,7 +16,7 @@ public class ObstacleFactory{
         int width = rand.nextInt(3-1) + 1;                  //nmax 3, nmin 1
         int lenght = rand.nextInt(6-2) + 2;                //nmax 6, nmin 2
         double speedx = rand.nextDouble() * (16.-12.) + 12.;   //nmax 16, nmin 12
-        double speedy = rand.nextDouble() * (16.-12.) + 12.;   //nmax 16, nmin 12
+        double speedy = rand.nextDouble() * (16.-12.);   //nmax 16, nmin 12
         double acc = rand.nextDouble() * (4.-.8) + .8;      //nmax 4, nmin 0.8
         int y = rand.nextInt(map.size()-5)+1;                //nmax 6, nmin 2
         Obstacle obs = new Obstacle(y , width, lenght, speedx, speedy , acc, map);
